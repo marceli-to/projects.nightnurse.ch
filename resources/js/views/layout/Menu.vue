4d389cf138fff447724a155a2a1fa08f547caf12
Marcel Stadelmann <m@marceli.to>
2022-02-24 09:11:10 +0100

    staging build

--- resources/js/views/layout/Menu.vue
+++ resources/js/views/layout/Menu.vue
@@ -32,14 +32,10 @@
       <li class="block !p-0 !m-0 !mt-6">
         <menu-item :route="'users'">
           <logout-icon class="h-5 w-5 sm:h-8 sm:w-8" aria-hidden="true" />
-          <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 sm:h-8 sm:w-8" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd" />
-          </svg> -->
         </menu-item>
       </li>
     </ul>
   </div>
-
   <!-- <div class="hidden sm:flex items-center px-4 pt-4">
     <div class="">
       <div class="flex items-center justify-center rounded-full bg-highlight text-white h-8 w-8">
