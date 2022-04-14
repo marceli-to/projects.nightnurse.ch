360ad0d126e71737e58fe72e37024e6fbf3b5406
Marcel Stadelmann <m@marceli.to>
2022-04-14 13:18:46 +0200

    staging build

--- resources/views/notifications/message.blade.php
+++ resources/views/notifications/message.blade.php
@@ -38,14 +38,16 @@
 @endforeach
 </div>
 @if ($message['users'])
-<div class="border-top pt-10 text-sm text-xs font-mono">
-Gesendet an:<br>
+<br>
+<div class="border-top border-bottom py-10 text-sm text-xs font-mono">
+Gesendet an: 
 @foreach($message['users'] as $user)
-<span class="text-xs">{{$user['full_name']}}@if (!$loop->last),</span> @endif
+<span class="text-xs font-mono">{{$user['full_name']}}@if (!$loop->last),</span> @endif
 @endforeach
 </div>
+<br>
 @endif
-<div class="pt-10 text-center text-sm font-mono">
+<div class="pt-20 text-center text-sm font-mono">
   <a href="{{url('')}}/projects/project/{{$message['project']['uuid']}}/messages" class="font-mono text-xs text-dark">Nachricht im Browser öffnen</a>
 </div>
 @endif
