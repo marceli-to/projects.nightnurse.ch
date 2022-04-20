134272ad18e71578307595a8944c1c0ea644a249
Marcel Stadelmann <m@marceli.to>
2022-04-20 06:43:37 +0200

    update error message

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -6,7 +6,7 @@
       @include('partials.logo')
     </div>
     <h2>Service unavailable 503</h2>
-    <p>The site is undergoing some maintenance. We'll be back shortly.</p>
+    <p>The site is undergoing maintenance. We'll be back shortly.</p>
   </div>
 </div>
 @endsection
