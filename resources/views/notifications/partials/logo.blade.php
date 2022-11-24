2b18b10187c826eeb920ae065cd72bd0fa257d93
Marcel Stadelmann <m@marceli.to>
2022-11-24 16:27:14 +0100

    updates logo

--- resources/views/notifications/partials/logo.blade.php
+++ resources/views/notifications/partials/logo.blade.php
@@ -1,3 +1,3 @@
 <a href="{{ env('APP_URL') }}" target="_blank" class="brand">
-  <img src="{{ env('APP_URL') }}/assets/img/logo.png" height="50" width="50" style="display: block; height: 50px; width: 50px">
+  <img src="{{ env('APP_URL') }}/assets/img/logo-nightnurse.png" height="55" width="50" style="display: block; height: 55px; width: 50px">
 </a>
