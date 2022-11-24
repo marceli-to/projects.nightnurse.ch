68ff7367cc8dfa8977785a3b8dd7a09c8d39e0b0
Marcel Stadelmann <m@marceli.to>
2022-11-24 16:29:50 +0100

    fix logo

--- resources/views/notifications/partials/logo.blade.php
+++ resources/views/notifications/partials/logo.blade.php
@@ -1,3 +1,3 @@
 <a href="{{ env('APP_URL') }}" target="_blank" class="brand">
-  <img src="{{ env('APP_URL') }}/assets/img/logo-nightnurse.png" height="55" width="50" style="display: block; height: 55px; width: 50px">
+  <img src="{{ env('APP_URL') }}/assets/img/logo-nightnurse.png" height="35" width="32" style="display: block; height: 35px; width: 32px">
 </a>
