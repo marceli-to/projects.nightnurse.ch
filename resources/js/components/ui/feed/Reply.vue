8158383534646f2e12ddb4d60b109be569b441f1
Marcel Stadelmann <m@marceli.to>
2022-12-16 18:23:35 +0100

    staging build

--- resources/js/components/ui/feed/Reply.vue
+++ resources/js/components/ui/feed/Reply.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="p-2 mb-2 lg:mb-4 border-2 border-zinc-100 text-sm sm:text-base text-dark relative rounded bg-zinc-50 group-[.is-internal]:bg-white group-[.is-private]:bg-white group-[.is-private]:border-slate-400 group-[.is-private]:text-slate-100">
-    <div :class="[originalMessage.body ? 'font-bold' : '', 'text-sm']" v-if="originalMessage.subject">
+    <div :class="[originalMessage.body ? 'font-bold' : '', 'text-sm italic']" v-if="originalMessage.subject">
       {{ originalMessage.subject }}
     </div>
-    <div class="text-sm" v-html="originalMessage.body"></div>
+    <div class="text-sm italic" v-html="originalMessage.body"></div>
   </div>
 </template>
 <script>
