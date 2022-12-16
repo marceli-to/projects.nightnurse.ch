7a10ea8acfa8e3e5a843fd24cf4388132948d210
Marcel Stadelmann <m@marceli.to>
2022-12-16 18:39:35 +0100

    update

--- resources/js/components/ui/feed/Reply.vue
+++ resources/js/components/ui/feed/Reply.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-2 mb-2 lg:mb-4 border-2 border-zinc-100 text-sm sm:text-base text-dark relative rounded bg-zinc-50 group-[.is-internal]:bg-white group-[.is-private]:bg-zinc-100 group-[.is-private]:border-slate-400 group-[.is-private]:text-slate-100">
+  <div class="p-2 mb-2 lg:mb-4 border-2 border-zinc-100 text-sm sm:text-base text-dark relative rounded bg-zinc-50 group-[.is-internal]:bg-white group-[.is-private]:bg-zinc-200 group-[.is-private]:border-slate-400 group-[.is-private]:text-slate-100">
     <div :class="[originalMessage.body ? 'font-bold' : '', 'text-sm italic']" v-if="originalMessage.subject">
       {{ originalMessage.subject }}
     </div>
