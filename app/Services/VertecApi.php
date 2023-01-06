83865078690b9eb7ef6f7ede8c09055b2be9980b
Marcel Stadelmann <m@marceli.to>
2023-01-06 11:17:12 +0100

    updates authorization for vertec api

--- app/Services/VertecApi.php
+++ app/Services/VertecApi.php
@@ -5,42 +5,15 @@ use App\Models\Project;
 
 class VertecApi
 { 
-  protected $username = 'deiters';
-  protected $password = '.heubergerweg8';
   protected $auth_url = 'https://nightnurse.vertec-mobile.com/auth/xml';
   protected $xml_url  = 'https://nightnurse.vertec-mobile.com/xml';
   private $token;
-
-  // public function __construct()
-  // {
-  //   $this->authorize();
-  // }
-
-  // public function authorize()
-  // {
-  //   $authxml = '&vertec_username='.$this->username.'&password='.$this->password;
-  //   $authcurl = curl_init($this->auth_url);
-  //   curl_setopt ($authcurl, CURLOPT_HTTPHEADER, array("Content-Type: text/xml"));
-  //   curl_setopt($authcurl, CURLOPT_POST, true);
-  //   curl_setopt($authcurl, CURLOPT_POSTFIELDS, $authxml);
-  //   curl_setopt($authcurl, CURLOPT_RETURNTRANSFER, true);
-  //   $token = curl_exec($authcurl);
-    
-  //   if(curl_errno($authcurl))
-  //   {
-  //     throw new Exception(curl_error($authcurl));
-  //   }
-  //   curl_close($authcurl);
-  //   $this->setToken($token);
-  //   return $token;
-  // }
 
   public function updateProject(Project $project)
   {
     $project = Project::with('manager')->findOrFail($project->id);
 
     $xml  = "<Envelope>";
-    $xml .= "<Header><BasicAuth><Token>".$this->getToken()."</Token></BasicAuth></Header>";
     $xml .= "<Body>";
     $xml .= "<Update><Projektphase>";
     $xml .= "<objref>{$project->vertec_id}</objref>";
@@ -67,7 +40,7 @@ class VertecApi
     $xml .= "</Envelope>";
 
     $curl = curl_init($this->xml_url);
-    curl_setopt ($curl, CURLOPT_HTTPHEADER, array("Content-Type: text/xml","Expect:"));
+    curl_setopt ($curl, CURLOPT_HTTPHEADER, array("Content-Type: text/xml", "Expect:", "Authorization: Bearer " . $this->getToken()));
     curl_setopt($curl, CURLOPT_POST, true);
     curl_setopt($curl, CURLOPT_POSTFIELDS, $xml);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
