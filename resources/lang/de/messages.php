1dec07b90e298812224f48fd233984b8958e4366
Marcel Stadelmann <m@marceli.to>
2023-03-17 21:05:55 +0100

    wip

--- resources/lang/de/messages.php
+++ resources/lang/de/messages.php
@@ -2,7 +2,7 @@
 
 return [
   'general_error' => 'Bitte alle Felder prüfen!',
-  'delete_cookies' => 'Sollten Sie Probleme beim Login haben, löschen Sie alle Cookies und versuchen Sie es erneut.',
+  'delete_cookies' => 'Sollten Sie Probleme beim Login haben, löschen Sie bitte alle Cookies und versuchen es erneut.',
   'password_recovery' => 'Halb so wild - alles was wir brauchen ist Ihre E-Mail und Sie erhalten einen Link um das Passwort zurücksetzen zu können.',
   'password_reset' => 'Fast geschafft - wir brauchen die E-Mail sowie ein neues Passwort sowie eine Bestätigung des neuen Passworts.'
 ];
