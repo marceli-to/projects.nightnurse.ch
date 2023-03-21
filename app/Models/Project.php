656cbacf81b0e101b421b6192d2f81aad42f2e57
Marcel Stadelmann <m@marceli.to>
2023-03-21 11:59:04 +0100

    wip

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -124,7 +124,7 @@ class Project extends Base
 
   public function getDateStartAttribute($value)
   {
-    return \Carbon\Carbon::parse($value)->format('d.m.Y');
+    return $value ? \Carbon\Carbon::parse($value)->format('d.m.Y') : '–';
   }
 
   /**
