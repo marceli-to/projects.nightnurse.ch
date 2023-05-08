2161f85b88e4e4ea8f9be34205efacc5be4efd31
Marcel Stadelmann <m@marceli.to>
2023-05-08 10:46:47 +0200

    wip

--- app/Console/Commands/Debug.php
+++ app/Console/Commands/Debug.php
@@ -40,7 +40,7 @@ class Debug extends Command
     // Folder: uploads/temp
     $files = \Storage::listContents('public/uploads/temp');
     collect($files)->each(function($file) {
-      dd($file);
+      dd($file['lastModified']);
       if (isset($file['timestamp']) && $file['timestamp'] < now()->subMinutes(30)->getTimestamp()) {
 
         //\Storage::delete($file['path']);
