2f096d1216cc77b0de22f6ca0c45d3bd8c91e11a
Marcel Stadelmann <m@marceli.to>
2023-05-08 11:53:55 +0200

    wip

--- app/Console/Commands/Debug.php
+++ app/Console/Commands/Debug.php
@@ -54,7 +54,7 @@ class Debug extends Command
       // Get all files in subfolder
       $files = \Storage::listContents($folder);
 
-      dd(collect($files)->empty());
+      dd(collect($files)->count());
 
       collect($files)->each(function($file) {
         // Delete files and folders older than 30 days
