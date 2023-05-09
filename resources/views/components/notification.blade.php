b9e0fc39fede23bce94326d3efcdb632e7e91741
Marcel Stadelmann <m@marceli.to>
2023-05-09 15:14:34 +0200

    wip

--- resources/views/components/notification.blade.php
+++ resources/views/components/notification.blade.php
@@ -1,10 +1,12 @@
-<div class="notification is-inline">
-<div>
-  @if ($notification->title)
-    <h1>{{ $notification->title }}</h1>
-  @endif
-  @if ($notification->text)
-    <div>{{ $notification->text }}</div>
-  @endif
-</div>
-</div>
+@if ($notification)
+  <div class="notification is-inline">
+    <div>
+      @if ($notification->title)
+        <h1>{{ $notification->title }}</h1>
+      @endif
+      @if ($notification->text)
+        <div>{{ $notification->text }}</div>
+      @endif
+    </div>
+  </div>
+@endif
