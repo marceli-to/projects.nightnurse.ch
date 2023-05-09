643c7f37afee103e97067bf480e72a8bd6496582
Marcel Stadelmann <m@marceli.to>
2023-05-09 18:15:42 +0200

    wip

--- resources/views/notifications/invitation.blade.php
+++ resources/views/notifications/invitation.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 @include('notifications.partials.logo')
 {{ $user->language->acronym}}
-<div class="text-md font-bold pt-20">{{ __('Einladung Project Room – Nightnurse', [], $user->language->acronym) }}</div>
+<div class="text-md font-bold pt-20">{{ __('Einladung Project Room – Nightnurse Images', [], $user->language->acronym) }}</div>
 <div class="pt-20 font-sm">{{ __('Um Ihre Registrierung abzuschliessen, klicken Sie bitte auf folgenden Link:') }}</div>
 <div class="pt-20 font-sm"><a href="{{url('')}}/register/{{$user['uuid']}}" class="button button-primary font-sm text-white">{{ __('Abschliessen') }}</a></div>
 <div class="pt-20 pb-20 font-sm">{{ __('Vielen Dank und bis bald!') }}</div>
