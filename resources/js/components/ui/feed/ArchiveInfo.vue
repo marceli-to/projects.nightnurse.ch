35b36cf7c2819acdc6603d28182c1fc9af521ec6
Marcel Stadelmann <m@marceli.to>
2023-05-11 17:18:47 +0200

    wip

--- resources/js/components/ui/feed/ArchiveInfo.vue
+++ resources/js/components/ui/feed/ArchiveInfo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="notification border-2 border-red-500 text-red-500 mb-4 mt-2 md:mb-0 md:!absolute inline-block w-auto right-0 leading-none p-2">
+  <div class="notification border-2 border-red-500 text-red-500 mb-4 mt-2 md:mb-0 md:!absolute inline-block w-auto right-0 leading-snug p-2">
     {{ translate('Projekt ist archiviert und wird in 30 Tagen gelöscht.') }}
   </div>
 </template>
