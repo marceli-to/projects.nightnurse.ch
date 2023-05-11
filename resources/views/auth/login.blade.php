23eabf457437b08e6fb9b5447f44a8fc10431407
Marcel Stadelmann <m@marceli.to>
2023-05-11 15:29:54 +0200

    wip

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 @section('seo_title', 'Login')
 @section('content')
 <x-card-auth>
-  <h2 class="card-auth__heading">Login</h2>
+  <h2 class="card-auth__heading">{{ __('Login') }}</h2>
   @if ($errors->any())
     <x-alert type="danger" message="{{__('messages.general_error')}}" />
   @endif
@@ -10,11 +10,11 @@
   <form method="POST" action="{{ route('login') }}">
     @csrf
     <x-text-field label="E-Mail" type="email" name="email" autocomplete="false" />
-    <x-text-field label="Passwort" type="password" name="password" autocomplete="false" />
+    <x-text-field label="{{ __('Passwort') }}" type="password" name="password" autocomplete="false" />
     <div class="card-auth__buttons">
-      <x-button label="Anmelden" name="register" type="submit" />
+      <x-button label="{{ __('Anmelden') }}" name="register" type="submit" />
       @if (Route::has('password.request'))
-        <a href="{{ route('password.request') }}" class="form-helper">Passwort vergessen?</a>
+        <a href="{{ route('password.request') }}" class="form-helper">{{ __('Passwort vergessen?') }}</a>
       @endif
     </div>
   </form>
