03cd2498cb3390c9c53c066be503c4dd9ccfd16a
Marcel Stadelmann <m@marceli.to>
2023-05-11 15:53:41 +0200

    wip

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -3,6 +3,6 @@
 return [
   'general_error' => 'Please check all fields!',
   'delete_cookies' => 'If you have problems logging in, delete all cookies and try again.',
-  'password_recovery' => 'No big deal - all we need is your email and you will receive a link to reset the password.',
+  'password_recovery' => 'No big deal - all we need is your email address and you will receive a link to reset the password.',
   'password_reset' => 'Almost there - we need the email as well as a new password and confirmation of the new password.'
 ];
