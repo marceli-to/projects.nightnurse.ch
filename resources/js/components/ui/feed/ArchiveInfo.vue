3deccdc41858bc50990b125b438e9a940e072ba0
Marcel Stadelmann <m@marceli.to>
2023-05-12 15:19:41 +0200

    wip

--- resources/js/components/ui/feed/ArchiveInfo.vue
+++ resources/js/components/ui/feed/ArchiveInfo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mt-4 mb-8 flex justify-center leading-6">
+  <div class="mt-4 mb-8 flex justify-center leading-7">
     <div class="notification inline-block p-2 w-auto border-2 border-red-500 text-red-500 max-w-sm">
       {{ translate('Dieses Projekt ist archiviert. Alle Dateien werden ungefähr 30 Tage nach Abschluss gelöscht.') }}
     </div>
