c00e58c9dfe0a563e2a43b0f16997568975af952
Marcel Stadelmann <m@marceli.to>
2023-07-10 17:14:45 +0200

    wip

--- resources/views/notifications/message.blade.php
+++ resources/views/notifications/message.blade.php
@@ -11,7 +11,7 @@
 <td>
 <div class="text-xs font-mono font-bold">
 {{$message['sender']['full_name']}}
-@if ({{$message['sender']['phone']}})
+@if ($message['sender']['phone'])
 <a href="tel:{{$message['sender']['phone']}}" class="text-xs font-mono font-bold no-underline">{{$message['sender']['phone']}}</a>
 @endif
 </div>
