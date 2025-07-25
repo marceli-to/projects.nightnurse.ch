a41b65a4178715d2f8cb2bf13f2c66ca31479108
Marcel Stadelmann <m@marceli.to>
2025-07-25 14:15:30 +0200

    cleans up message/reply to handling

--- app/Mail/MessageNotification.php
+++ app/Mail/MessageNotification.php
@@ -29,20 +29,27 @@ class MessageNotification extends Mailable
    */
   public function build()
   {
-    // make sure we don't use the generic reply-to address in production (yet :-))
-    if (env('APP_ENV') == 'production')
-    {
-      return $this->from(env('MAIL_FROM_ADDRESS'), 'Nightnurse Images - Project Room')
-            ->replyTo($this->message->sender->email)
-            ->subject($this->subject)
-            ->with(['message' => $this->message])
-            ->markdown('notifications.message');
-    }
-
     return $this->from(env('MAIL_FROM_ADDRESS'), 'Nightnurse Images - Project Room')
-          ->replyTo('nni+' . $this->message->uuid . '@marceli.to')
-          ->subject($this->subject)
-          ->with(['message' => $this->message])
-          ->markdown('notifications.message');
+      ->replyTo($this->message->sender->email)
+      ->subject($this->subject)
+      ->with(['message' => $this->message])
+      ->markdown('notifications.message');
+
+    // TODO: remove this once we have a proper reply-to address
+    // make sure we don't use the generic reply-to address in production (yet :-))
+    // if (env('APP_ENV') == 'production')
+    // {
+    //   return $this->from(env('MAIL_FROM_ADDRESS'), 'Nightnurse Images - Project Room')
+    //         ->replyTo($this->message->sender->email)
+    //         ->subject($this->subject)
+    //         ->with(['message' => $this->message])
+    //         ->markdown('notifications.message');
+    // }
+
+    // return $this->from(env('MAIL_FROM_ADDRESS'), 'Nightnurse Images - Project Room')
+    //       ->replyTo('nni+' . $this->message->uuid . '@marceli.to')
+    //       ->subject($this->subject)
+    //       ->with(['message' => $this->message])
+    //       ->markdown('notifications.message');
   }
 }
