031b948abaf8c2ee6b8728281034af772fd8f332
Marcel Stadelmann <m@marceli.to>
2025-07-25 15:42:37 +0200

    adds indexes to comments

--- resources/js/components/ui/markup/Comment.vue
+++ resources/js/components/ui/markup/Comment.vue
@@ -2,7 +2,7 @@
   <a href="javascript:;" 
     @mouseover="$emit('mouseover', comment.uuid)" 
     @mouseout="$emit('mouseout')"
-    :class="[highlighted == comment.uuid ? 'bg-gray-100' : '', 'no-underline text-gray-400 flex mb-4 lg:mb-6 p-1 lg:p-2 rounded-md w-full']">
+    :class="[highlighted == comment.uuid ? 'bg-gray-100' : '', 'no-underline text-gray-400 flex mb-3 lg:mb-3 p-1 lg:p-2 rounded-md w-full']">
     <div class="shrink-0 relative -top-2">
       <span class="font-bold rounded-full w-[20px] h-[20px] inline-flex items-center justify-center leading-none text-xs bg-gray-300 text-white">
         {{ commentNumber }}
@@ -17,7 +17,7 @@
         <template v-if="$store.state.user.admin && !comment.is_done">
           <button 
             @click.stop="markAsDone"
-            class="text-xs block text-highlight hover:text-dark transition-colors duration-200 mt-2">
+            class="text-xs block text-highlight hover:text-dark transition-colors duration-200 mt-1">
             {{ translate('Erledigt') }}
           </button>
         </template>
